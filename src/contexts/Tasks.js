e554dcd881ddec41b12a3d2a996db9d6969c72c6
Krzaczor <jbielecki18@gmail.com>
2019-11-26 17:39:09 +0100

    add gh-pages

--- src/contexts/Tasks.js
+++ src/contexts/Tasks.js
@@ -2,14 +2,9 @@ import React from 'react';
 
 export const Tasks = React.createContext({
     tasks: [],
-    tasksEdit: [],
     addTask() { },
-    doneTask() { },
     doneTasks() { },
-    removeTask() { },
     removeTasks() { },
-    toggleTaskEdit() { },
-    resetTaskEdit() { }
 });
 
 Tasks.displayName = "Tasks";
--- src/contexts/TasksProvider.js
+++ src/contexts/TasksProvider.js
@@ -10,111 +10,40 @@ class TasksProvider extends React.Component {
         super(props);
         this.state = {
             tasks: ls.get('tasks') || [],
-            tasksEdit: [],
             addTask: this.addTask,
-            doneTask: this.doneTask,
             doneTasks: this.doneTasks,
-            removeTask: this.removeTask,
-            removeTasks: this.removeTasks,
-            toggleTaskEdit: this.toggleTaskEdit,
-            resetTaskEdit: this.resetTaskEdit
+            removeTasks: this.removeTasks
         }
     }
 
     addTask = (contentNextTask) => {
         let idNextTask;
-        let tasks = [];
 
         do {
             idNextTask = nanoid();
-        } while (this.state.tasks.some(task => task.id === idNextTask));
+        } while (
+            this.state.tasks.map(item => item.id).includes(idNextTask)
 
-        tasks = [...this.state.tasks, {
-            id: idNextTask,
-            content: contentNextTask,
-            done: false,
-            created: new Date().getTime()
-        }]
+            /**
+             * this.state.tasks.some(task => task.id === idNextTask)
+             * 
+             * wcześniejszy zapis sprawdzania czy id się powtarza
+             * Reactowy komunikat: Function declared in a loop contains unsafe references to variable(s) 'idNextTask'
+             */
+        );
 
-        this.setState({
-            tasks
-        }, () => {
-            ls.set('tasks', tasks);
-        });
-    }
-
-    doneTask = (idTask) => {
-        const tasks = this.state.tasks.map(task => {
-            if (task.id === idTask) {
-                task.done = true;
-            }
-
-            return task;
-        });
-
-        this.setState({
-            tasks
-        }, () => {
-            ls.set('tasks', tasks);
-        });
-    }
-
-    doneTasks = () => {
-        let change = false;
-        const tasks = this.state.tasks.map(task => {
-            if (this.state.tasksEdit.includes(task.id)) {
-                if (task.done === false) {
-                    task.done = true;
-                    change = true;
-                }
-            }
-
-            return task;
-        });
-
-        if (change) {
-            this.setState({
-                tasks
-            }, () => {
-                ls.set('tasks', tasks);
-            });
-        }
-    }
-
-    removeTask = (idTask) => {
-        this.setState((prevState) => {
+        this.setState(prevState => {
             return {
-                tasks: prevState.tasks.filter(task => task.id !== idTask)
+                tasks: [...prevState.tasks, {
+                    id: idNextTask,
+                    content: contentNextTask,
+                    done: false,
+                    created: new Date().getTime()
+                }]
             }
         });
 
         ls.set('tasks', this.state.tasks);
-    }
-
-    removeTasks = () => {
-        const tasks = this.state.tasks.filter(task => !this.state.tasksEdit.includes(task.id))
-
-        this.setState({
-            tasks
-        }, () => {
-            ls.set('tasks', tasks);
-        });
-    }
-
-    toggleTaskEdit = (idTask) => {
-        let tasks = this.state.tasksEdit;
-
-        this.setState({
-            tasksEdit: tasks.includes(idTask)
-                ? tasks.filter(id => id !== idTask)
-                : [...tasks, idTask]
-        });
-    }
-
-    resetTaskEdit = () => {
-        this.setState({
-            tasksEdit: []
-        });
     }
 
     render() {
