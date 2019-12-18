10936552447b89be72124237470d728f624243b0
Krzaczor <jbielecki18@gmail.com>
2019-12-18 04:22:56 +0100

    add manifest

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
 import { Provider } from 'react-redux';
 import store from './mainStore';
 import App from './App';
 
-import './index.css';
 
 ReactDOM.render(<Provider store={store}>
   <App />
 </Provider>, document.getElementById('root'));
+
+serviceWorker.unregister();
