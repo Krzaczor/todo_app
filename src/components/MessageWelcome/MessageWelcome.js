77f01a59b5052cad58dd14e7a789366461455b8f
unknown <jbielecki18@gmail.com>
2021-05-16 03:52:52 +0200

    center quotation and block overflow-x

--- src/components/MessageWelcome/MessageWelcome.js
+++ src/components/MessageWelcome/MessageWelcome.js
@@ -5,13 +5,15 @@ import Quotation from './Quotation';
 const MessageWelcomeWrapper = styled.div`
     position: fixed;
     top: 50%;
-    transform: translateY(-50%);
+    left: 50%;
+    transform: translate(-50%, -50%);
     padding: 0 3em;
+    width: 90%;
     text-align: center;
-    /* display: flex; */
-    /* flex-direction: column;
-    align-items: center; */
-    /* justify-content: center; */
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
 `;
 
 MessageWelcomeWrapper.displayName = 'MessageWelcome';
